2655cba5d2f802595ea71680e17cec48964dd3b1
Sevrinn <sevrinnsevrinn@gmail.com>
2022-05-13 00:10:26 -0700

    post request tested and working!

--- server.js
+++ server.js
@@ -1,29 +1,37 @@
-const express = require('express')
-const path = require("path")
-const app = express()
+const express = require('express');
+const path = require("path");
+const app = express();
 const port = 8000;
 
 
 
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 app.listen(port, () => {
-  console.log(`Listening at http://localhost:${port}`)
-})
+  console.log(`Listening at http://localhost:${port}`);
+});
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "./public/index.html"))
+  res.sendFile(path.join(__dirname, "./public/index.html"));
 })
 
 const strCutter = (str) => {
   let returnStr = "";
   for (let i = 2; i < str.length; i += 3) {
-    returnStr += str[i]
+    returnStr += str[i];
   }
-  return returnStr
-}
+  return {return_string: returnStr};
+};
 
-let strToCut = "iamyourlyftdriver"
+app.post("/test", (req, res) => {
+  const {string_to_cut} = req.body;
+  if (string_to_cut && typeof string_to_cut === "string") {
+    res.json(strCutter(string_to_cut));
+  } else {
+    res.status(400).send("Something's not right with this post req");
+  }
+  res.end();
+})
 
-console.log(strCutter(strToCut))
+
 
