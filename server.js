f7b2946d36346b345da12bf46601333e4380272f
Sevrinn <sevrinnsevrinn@gmail.com>
2022-05-12 23:34:47 -0700

    get request tested sends html file

--- server.js
+++ server.js
@@ -1,9 +1,29 @@
 const express = require('express')
+const path = require("path")
 const app = express()
 const port = 8000;
+
+
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}`)
 })
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"))
+})
+
+// const strCutter = (str) => {
+//   let returnStr = "";
+//   for (let i = 2; i < str.length; i += 3) {
+//     returnStr += str[i]
+//   }
+//   return returnStr
+// }
+
+// let strToCut = "iamyourlyftdriver"
+
+// console.log(strCutter(strToCut))
+
